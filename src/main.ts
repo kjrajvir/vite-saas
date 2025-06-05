c31d33b03cd332d1e1d30f323f9b6e65ea18b4de
kjrajvir <kalpeshrajvir@gmail.com>
2025-06-05 23:00:27 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import router from './router';
 import App from './App.vue'
 
 import { vuetify } from '@/plugins/vuetify';
@@ -7,4 +8,5 @@ import { vuetify } from '@/plugins/vuetify';
 
   const app = createApp(App)
   app.use(vuetify);
+  app.use(router);
   app.mount('#app')
